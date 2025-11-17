ab9b8e95277ec97b359f9c315900a84d4b4c54c3
Ben Monaghan <monoganog@gmail.com>
2025-11-17 18:27:14 +0000

    Add new variables for configuration

--- variables.tf
+++ variables.tf
@@ -3,3 +3,18 @@ variable "length" {
   type        = number
   default     = 16
 }
+
+variable "alpha" {
+  description = "Test"
+  type        = number
+}
+
+variable "charlie" {
+  description = "Test"
+  type        = number
+}
+
+variable "bravo" {
+  description = "Test"
+  type        = string
+}
